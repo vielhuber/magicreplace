873e501caffa06efed7c728824e4b7ca1cab8695
David Vielhuber <david@vielhuber.de>
2017-03-21 01:52:00 +0100

    .

--- src/magicreplace.php
+++ src/magicreplace.php
@@ -61,39 +61,18 @@ class magicreplace
 		return $data;
 	}
 }
-
 // cli usage
-if (php_sapi_name() == 'cli' && isset($argv) && !empty($argv) && isset($arvg[1]))
-{
-	if (!isset($argv) || empty($argv) || !isset($argv[1]) || !isset($argv[2]) || !isset($argv[3]) || !isset($argv[4]))
-	{
-		die('missing options');
-	}
-	if (!file_exists(getcwd() . '/' . $argv[1]))
-	{
-		die('missing input');
-	}
+if (php_sapi_name() == 'cli' && isset($argv) && !empty($argv) && isset($arvg[1])) {
+	if (!isset($argv) || empty($argv) || !isset($argv[1]) || !isset($argv[2]) || !isset($argv[3]) || !isset($argv[4])) { die('missing options'); }
+	if (!file_exists(getcwd() . '/' . $argv[1])) { die('missing input'); }
 	$input = getcwd() . '/' . $argv[1];
-	if (!file_exists(getcwd() . '/' . $argv[2]))
-	{
-		touch(getcwd() . '/' . $argv[2]);
-	}
+	if (!file_exists(getcwd() . '/' . $argv[2])) { touch(getcwd() . '/' . $argv[2]); }
 	$output = getcwd() . '/' . $argv[2];
 	$search_replace = [];
-	foreach ($argv as $argv__key => $argv__value)
-	{
-		if ($argv__key <= 2)
-		{
-			continue;
-		}
-		if ($argv__key % 2 == 1 && !isset($argv[ $argv__key + 1 ]))
-		{
-			continue;
-		}
-		if ($argv__key % 2 == 0)
-		{
-			continue;
-		}
+	foreach ($argv as $argv__key => $argv__value) {
+		if ($argv__key <= 2) { continue; }
+		if ($argv__key % 2 == 1 && !isset($argv[ $argv__key + 1 ])) { continue; }
+		if ($argv__key % 2 == 0) { continue; }
 		$search_replace[ $argv[ $argv__key ] ] = $argv[ $argv__key + 1 ];
 	}
 	magicreplace::run($input, $output, $search_replace);
