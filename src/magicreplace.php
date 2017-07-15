f3b8f1f3e029a95b6e5d18ba4f8bdf300f5ead3d
David Vielhuber <david@vielhuber.de>
2017-07-16 00:26:42 +0200

    cleanup

--- src/magicreplace.php
+++ src/magicreplace.php
@@ -2,9 +2,11 @@
 namespace vielhuber\magicreplace;
 class magicreplace
 {
-	public static function run($input, $output, $search_replace) {
+	public static function run($input, $output, $search_replace)
+	{
 		if( !file_exists($input) ) { die('error'); } $data = file_get_contents($input);
-		foreach($search_replace as $search_replace__key=>$search_replace__value) {
+		foreach($search_replace as $search_replace__key=>$search_replace__value)
+		{
 		    // first find all critical serialized occurences in a very fast and efficient way
 		    preg_match_all('/'.preg_quote($search_replace__key, '/').'.*\"\;/', $data, $positions, PREG_OFFSET_CAPTURE);
 		    $position_offset = 0;
@@ -29,18 +31,22 @@ class magicreplace
 
 		        // there is another special case: sometimes there are references inside arrays (that cannot be preserved)
 		        // therefore we first resolve those references (by calling the whole procedure with an empty replace
-		        if( strpos($string_before,'R:') !== false && (!isset($numbers_before[0]) || !isset($numbers_after[0]) || count($numbers_before[0]) != count($numbers_after[0])) ) {
+		        if( strpos($string_before,'R:') !== false && (!isset($numbers_before[0]) || !isset($numbers_after[0]) || count($numbers_before[0]) != count($numbers_after[0])) )
+		        {
 			        $string_before = self::string($string_before, ['NONE'=>'NONE']);
 					preg_match_all('/s:\d+:/', $string_before, $numbers_before, PREG_OFFSET_CAPTURE);
 					$string_final = $string_before;
 		    	}
 
 				// something went wrong: replace search term temporarily (is changed later on again)
-				if( !isset($numbers_before[0]) || !isset($numbers_after[0]) || count($numbers_before[0]) != count($numbers_after[0]) ) {
+				if( !isset($numbers_before[0]) || !isset($numbers_after[0]) || count($numbers_before[0]) != count($numbers_after[0]) )
+				{
 					$string_final = str_replace($search_replace__key, md5($search_replace__key.(strlen($search_replace__key)*42)), $string_final);
 				}
-				else {
-					foreach($numbers_before[0] as $numbers_before__key=>$numbers_before__value) {
+				else
+				{
+					foreach($numbers_before[0] as $numbers_before__key=>$numbers_before__value)
+					{
 					    if( $numbers_before__value[0] == $numbers_after[0][$numbers_before__key][0] ) { continue; }
 					    $numbers_begin = $numbers_before__value[1]+$numbers_offset;
 					    $numbers_end = $numbers_before__value[1]+$numbers_offset+strlen($numbers_before__value[0]);
@@ -61,28 +67,25 @@ class magicreplace
 		file_put_contents($output, $data);
 	}
 
-	public static function mask($data) {
+	public static function mask($data)
+	{
 		$data = str_replace('\\\\"',md5('NOREPLACE1'),$data);
 		$data = str_replace('\\\\n',md5('NOREPLACE2'),$data);
-		$data = str_replace('\\\\r',md5('NOREPLACE3'),$data);
-		
-		$data = str_replace('\n',"\n",$data);
-					
+		$data = str_replace('\\\\r',md5('NOREPLACE3'),$data);		
+		$data = str_replace('\n',"\n",$data);					
 		$data = str_replace('\r',"\r",$data);
-
 		$data = str_replace('\\\\','\\',$data);
 		$data = str_replace('\'\'','\'',$data);			
 		$data = str_replace('\\\'','\'',$data);
 		$data = str_replace('\\"','"',$data);
-
 		$data = str_replace(md5('NOREPLACE3'),'\\r',$data);
 		$data = str_replace(md5('NOREPLACE2'),'\\n',$data);
 		$data = str_replace(md5('NOREPLACE1'),'\\"',$data);
-
 		return $data;
 	}
 
-	public static function string($data, $search_replace, $serialized = false, $level = 0) {
+	public static function string($data, $search_replace, $serialized = false, $level = 0)
+	{
 		// special case: if data is boolean false (unserialize would return false)
 		if( $data === 'b:0;' ) { $data = self::string(unserialize($data), $search_replace, true, $level+1); }
 		// special case: class cannot be unserialized (sometimes yoast serializes data at runtime when a class is available), return empty string
@@ -91,17 +94,20 @@ class magicreplace
 		elseif( @unserialize($data) !== false ) { $data = self::string(unserialize($data), $search_replace, true, $level+1); }
 		// special case: if data contains new lines and is recognized after replacing them AND/OR if data contains double quotes and is recognized after replacing them
 		elseif( is_string($data) && @unserialize(self::mask($data)) !== false ) { $data = self::string(unserialize(self::mask($data)), $search_replace, true, $level+1); }
-		elseif( is_array($data) ) {
+		elseif( is_array($data) )
+		{
 			$tmp = [];
 			foreach ( $data as $data__key => $data__value ) { $tmp[ $data__key ] = self::string( $data__value, $search_replace, false, $level+1 ); }
 			$data = $tmp; unset( $tmp );
 		}
-		elseif ( is_object( $data ) ) {
+		elseif ( is_object( $data ) )
+		{
 			$tmp = $data; $props = get_object_vars( $data );
 			foreach ( $props as $data__key => $data__value ) { $tmp->$data__key = self::string( $data__value, $search_replace, false, $level+1 ); }
 			$data = $tmp; unset( $tmp );
 		}
-		elseif( is_string($data) ) {
+		elseif( is_string($data) )
+		{
 			foreach($search_replace as $search_replace__key => $search_replace__value ) { $data = str_replace($search_replace__key, $search_replace__value, $data); }
 		}
 		if( $serialized === true ) { return serialize($data); }
@@ -109,7 +115,8 @@ class magicreplace
 	}
 }
 // cli usage
-if (php_sapi_name() == 'cli' && isset($argv) && !empty($argv) && isset($argv[1])) {
+if (php_sapi_name() == 'cli' && isset($argv) && !empty($argv) && isset($argv[1]))
+{
 	if (!isset($argv) || empty($argv) || !isset($argv[1]) || !isset($argv[2]) || !isset($argv[3]) || !isset($argv[4])) { die('missing options'); }
 	$root = getcwd() . '/';
 	if (!file_exists($root . $argv[1])) { $root = ''; }
@@ -118,23 +125,21 @@ if (php_sapi_name() == 'cli' && isset($argv) && !empty($argv) && isset($argv[1])
 	if (!file_exists($root . $argv[2])) { touch($root . $argv[2]); }
 	$output = $root . $argv[2];
 	$search_replace = [];
-	foreach ($argv as $argv__key => $argv__value) {
+	foreach ($argv as $argv__key => $argv__value)
+	{
 		if ($argv__key <= 2) { continue; }
 		if ($argv__key % 2 == 1 && !isset($argv[ $argv__key + 1 ])) { continue; }
 		if ($argv__key % 2 == 0) { continue; }
 		$search_replace[ $argv[ $argv__key ] ] = $argv[ $argv__key + 1 ];
 	}
-
 	// split source file in several files
 	exec('split '.$input.' -b 100m '.$input.'-SPLITTED');
-
-	foreach( glob($input.'-SPLITTED*') as $filename ) {
+	foreach( glob($input.'-SPLITTED*') as $filename )
+	{
 		magicreplace::run($filename, $filename, $search_replace);
 	}
-
 	// join files
 	exec('cat '.$input.'-SPLITTED* > '.$output);
 	exec('rm '.$input.'-SPLITTED*');
-
 	die('done...');
 }
